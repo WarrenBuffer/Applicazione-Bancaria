5459612ee3f4ab13fa32e6d9da5d39a0f1415498
Gabriele Salvatore Vazzana <vazzanag@int.master.lan>
2024-07-17 16:19:23 +0200

    bono

---
+++ src/main/java/com/tas/applicazionebancaria/businesscomponent/model/MovimentiContoMongo.java
@@ -0,0 +1,30 @@
+package com.tas.applicazionebancaria.businesscomponent.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+@Document(collection = "operazioni_bancarie")
+
+// --- classe mongo equilvalente alla classe MovimentiConto MySQL
+
+public class MovimentiContoMongo implements Serializable{
+	
+	private static final long serialVersionUID = 3589150116495049789L;
+	
+	@Id
+	private String codOperazione;
+	
+	private double importo;
+	private Date dataOperazione;
+	private String tipoOperazione;
+	private int codContoOrigine;
+	private int codContoDestinazione;
+}
---
+++ src/main/java/com/tas/applicazionebancaria/businesscomponent/model/RichiestePrestito.java
@@ -0,0 +1,46 @@
+package com.tas.applicazionebancaria.businesscomponent.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "richieste_prestito")
+public class RichiestePrestito implements Serializable {
+
+	private static final long serialVersionUID = -8584632604811064924L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codRichiesta;
+
+	@Column(nullable = false)
+	private double importo;
+	@Column(nullable = false)
+	private Date dataRichiesta;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private statoEnum stato;
+	
+	@Column(nullable = false)
+	@ManyToOne
+	@JoinColumn(name = "codCliente", nullable = false)
+	private Cliente codCliente;
+}
+
+enum statoEnum {
+	IN_ATTESA, APPROVATO, RIFIUTATO
+}
