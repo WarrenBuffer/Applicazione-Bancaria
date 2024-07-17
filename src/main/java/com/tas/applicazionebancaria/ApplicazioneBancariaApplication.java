598dc651a3da6226864e52003567f4aa3ba34313
Pierpaolo Feltrin <feltrinp@int.master.lan>
2024-07-17 10:30:15 +0200

    Ready, Set, Go!

---
+++ src/main/java/com/tas/applicazionebancaria/ApplicazioneBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.tas.applicazionebancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazioneBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazioneBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tas/applicazionebancaria/ApplicazioneBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tas.applicazionebancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicazioneBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
