ebd0123331f0e90bff57aca6f2a342495647294a
Pierpaolo Feltrin <feltrinp@int.master.lan>
2024-07-17 11:42:34 +0200

    Aggiornato application.properties e pom.xml

--- sql/script.sql
+++ sql/script.sql
@@ -1,3 +1,6 @@
 -- DA ADMIN
 create user 'project_admin'@'localhost' identified by 'pass';
 create database applicazione_bancaria;
+GRANT ALL PRIVILEGES
+ON applicazione_bancaria.*
+TO 'project_admin'@'localhost';
