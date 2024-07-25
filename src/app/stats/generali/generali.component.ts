910e430f7ed7b3051e4cb14df34be302024637a1
Pierpaolo Feltrin <feltrinp@int.master.lan>
2024-07-25 12:15:59 +0200

    Risolto bug statistiche se non ci sono clienti presenti

--- src/app/stats/generali/generali.component.ts
+++ src/app/stats/generali/generali.component.ts
@@ -6,22 +6,83 @@ import { Cliente } from '../../model/cliente';
   templateUrl: './generali.component.html',
   styleUrl: './generali.component.css'
 })
-export class GeneraliComponent implements OnInit{
+export class GeneraliComponent implements OnInit {
   @Input() stats: any;
 
   transazioniPerMese = new Array<number>(12).fill(0);
   tableTransactions = [this.transazioniPerMese];
   intervalli = new Array<number>(5).fill(0);
   tableIntervalli = [this.intervalli];
+  clienteSaldoPiuAlto: string = 'Nessun cliente presente';
+  ultimaTransazione: Date = new Date('1800-01-01T00:00:00');
+  totNTransazioni: number = 0;
+  totImportoTransazioni: number = 0;
+  saldoMedioConti: number = 0;
+  contiPositivi: number = 0;
+  contiNegativi: number = 0;
+  saldoTotale: number = 0;
 
   ngOnInit(): void {
     this.stats.importoTransazioniPerMese.forEach((mese: any) => {
       this.transazioniPerMese[Number(mese.id) - 1] = mese.importo;
     });
-    this.calcClientPerIntervallo();
+    this.calcStats();
   }
 
-  calcClientPerIntervallo() {
+
+  calcStats() {
+    let conti = 0;
+    this.stats.clienti.forEach((cliente: any) => {
+      let tmpMaxSaldo = 0;
+
+      cliente.conti.forEach((conto: any) => {
+        let saldo = 0;
+        conti ++;
+
+        conto.transazioni.forEach((transazione: any) => {
+          saldo += conto.saldo;
+          this.saldoTotale += conto.saldo;
+
+          this.totNTransazioni++;
+          this.totImportoTransazioni += transazione.importo;
+
+          const tmpDate = new Date(transazione.dataTransazione);
+          if (tmpDate >= this.ultimaTransazione) this.ultimaTransazione = tmpDate;
+        })
+
+        conto.transazioniBancarie.forEach((transazione: any) => {
+          this.totNTransazioni++;
+          this.totImportoTransazioni += transazione.importo;
+
+          const tmpDate = new Date(transazione.dataTransazione);
+          if (tmpDate >= this.ultimaTransazione) this.ultimaTransazione = tmpDate;
+        })
+
+        // cliente con saldo più alto
+        if (saldo >= tmpMaxSaldo) {
+          this.clienteSaldoPiuAlto = cliente.emailCliente;
+          tmpMaxSaldo = saldo;
+        }
+
+        // clienti per intervallo di saldo
+        if (saldo <= 1000) this.intervalli[0]++;
+        else if (saldo <= 5000) this.intervalli[1]++;
+        else if (saldo <= 25000) this.intervalli[2]++;
+        else if (saldo <= 100000) this.intervalli[3]++;
+        else this.intervalli[4]++;
+
+        // clienti saldo positivo/negativo
+        saldo >= 0 ? this.contiPositivi ++ : this.contiNegativi ++;
+
+      })
+
+      // saldo medio conti
+      conti !== 0 ? this.saldoMedioConti = this.saldoTotale / conti : this.saldoMedioConti = 0;
+
+    })
+  }
+
+/*   calcClientPerIntervallo() {
     this.stats.clienti.forEach((cliente: any) => {
       let saldo = 0;
       cliente.conti.forEach((conto: any) => {
@@ -69,7 +130,7 @@ export class GeneraliComponent implements OnInit{
     })
     return ultimaTransazione;
   }
-  
+
   calcTotTransazioni(clienti: any): string {
     let totTransazioni = 0;
     let totImporto = 0;
@@ -113,7 +174,7 @@ export class GeneraliComponent implements OnInit{
         saldo += conto.saldo;
       })
       if (saldo >= 0) positivi++;
-      else negativi ++;
+      else negativi++;
     })
 
     return `${positivi} - ${negativi}`;
@@ -129,5 +190,5 @@ export class GeneraliComponent implements OnInit{
     })
 
     return saldo;
-  }
+  } */
 }
